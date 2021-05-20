4aae49afbf6a3b75b548f2c3bd485eadb962dfed
Red <etonseller@gmail.com>
2021-05-20 09:56:42 +0200

    main game start mod

--- src/main/java/brszta/minesweeper/Main.java
+++ src/main/java/brszta/minesweeper/Main.java
@@ -108,9 +108,6 @@ public class Main {
                     if(controller.isConnected()){
                         if(controller.isNewGame()){
 
-                            while (client.getInputGame().getBoard() == null){
-
-                            }
                             game.setBoard(client.getInputGame().getBoard());
                             controller.setNewBoard(false);
                             controller.setRunning(true);
